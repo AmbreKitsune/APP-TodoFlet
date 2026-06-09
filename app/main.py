763ac1b6ef6f043ab83abd0730fa46971d3fd13f
AmbreKitsune <shadowplay746@gmail.com>
2026-06-09 11:30:05 +0500

    WIP-0.2

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import flet as ft
 from app.models.task import Task
 
+
 def application(page: ft.Page):
     page.title = "Todo App"
 
     page.window.width = 600
     page.window.height = 400
 
+
+if __name__ == "__main__":
     task = Task(id="1", title=" Купить хлеб ")
     print(task.model_dump())
 
-
-ft.run(application)
+    ft.run(application)
--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, field_validator
 
+
 class Task(BaseModel):
     id: str
     title: str
