16db8c7c2cef5c844f1f7ce552603a58b43487d3
AmbreKitsune <shadowplay746@gmail.com>
2026-06-09 11:08:22 +0500

    WIP-0.1

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 import flet as ft
-from application import application
+
+def application(page: ft.Page):
+    page.title = "Todo App"
+
+    page.window.width = 600
+    page.window.height = 400
+
 
 ft.run(application)
