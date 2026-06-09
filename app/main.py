9a99c221802f4b0c37f76a74283cc2b0b52a4b36
AmbreKitsune <shadowplay746@gmail.com>
2026-06-09 11:24:28 +0500

    WIP-0.2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import flet as ft
+from app.models.task import Task
 
 def application(page: ft.Page):
     page.title = "Todo App"
 
     page.window.width = 600
     page.window.height = 400
+
+    task = Task(id="1", title=" Купить хлеб ")
+    print(task.model_dump())
 
 
 ft.run(application)
---
+++ app/models/task.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, field_validator
+
+class Task(BaseModel):
+    id: str
+    title: str
+
+    @field_validator("title")
+    def clean_spaces(cls, v: str) -> str:
+        cleaned = " ".join(v.split())
+
+        if not cleaned:
+            raise ValueError("Title is empty")
+
+        return cleaned
