ec93c9639ed34f231282deb4ceeb45a73a02238e
AmbreKitsune <shadowplay746@gmail.com>
2026-06-09 14:30:42 +0500

    WIP-0.5.1

--- app/ui/todo_app.py
+++ app/ui/todo_app.py
@@ -12,14 +12,16 @@ class TodoApp:
         column_layout = ft.Column(
             controls=[
                 ft.Text("Todo App"),
-                ft.TextField("Введите текст"),
+                ft.TextField(label="Введите текст"),
                 ft.ElevatedButton("Добавить задачу"),
                 ft.Text("Счётчик задачи: 0")
             ]
         )
 
         task_list = ft.Column(
-            controls=[]
+            controls=[
+                ft.Text("Задач пока нет")
+            ]
         )
 
         self.page.add(column_layout, task_list)
